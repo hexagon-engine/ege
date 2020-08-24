7876266d8b4e0b6e93947a1068c04563db3ece27
Maciej Zygmanowski <sppmacd@pm.me>
2020-08-24 16:22:20 +0200

    scene: Added camera scaling modes
    The scaling modes defines, how many screen will remaing visible
    when resizing window. The default is None, which resizes camera
    along with window (the view is not scaled). You can scale view
    to remain fully visible in X axis, Y axis or both.

--- ege/scene/CameraObject.h
+++ ege/scene/CameraObject.h
@@ -10,6 +10,16 @@ Copyright (c) Sppmacd 2020
 namespace EGE
 {
 
+enum class ScalingMode
+{
+    None, // the default, no scaling
+    Centered, // the camera display is centered on scene widget, but not scaled
+    Fit, // screen is scaled to specified SIZE, but keep proportions
+    XLocked, // the x coord is always locked to specified FOV value
+    YLocked, // the y coord is always locked to specified FOV value
+    Scaled, // the view is forced to be scaled to specified SIZE ("manual mode")
+};
+
 // requirements for CoordType:
 //  must have default constructor public
 template<class CoordType>
@@ -21,8 +31,41 @@ public:
 
     CoordType getEyePosition() { return CoordType(); };
 
+    void setScalingMode(ScalingMode mode)
+    {
+        m_scalingMode = mode;
+    }
+
+    ScalingMode getScalingMode()
+    {
+        return m_scalingMode;
+    }
+
+    void setFOV(float fov)
+    {
+        m_fov = fov;
+    }
+
+    float getFOV() const
+    {
+        return m_fov;
+    }
+
+    void setSize(CoordType size)
+    {
+        m_size = size;
+    }
+
+    CoordType getSize()
+    {
+        return m_size;
+    }
+
 protected:
+    float m_fov;
+    CoordType m_size;
     Scene* m_parent;
+    ScalingMode m_scalingMode;
 };
 
 }
--- ege/scene/Scene2D.cpp
+++ ege/scene/Scene2D.cpp
@@ -21,7 +21,57 @@ void Scene2D::renderOnly(sf::RenderTarget& target)
     if(!m_camera.expired())
     {
         sf::View view = target.getView();
+        CameraObject2D* cam = m_camera.lock().get();
         view.setCenter(m_camera.lock()->getEyePosition());
+        sf::Vector2f _size;
+
+        switch(cam->getScalingMode())
+        {
+        case ScalingMode::None:
+            {
+                _size = getSize();
+
+                // The SFML camera is by default centered, so "uncenter" it.
+                view.move(getSize() / -2.f);
+            }
+            break;
+        case ScalingMode::Centered:
+            {
+                _size = getSize();
+            }
+            break;
+        case ScalingMode::Fit:
+            {
+                float aspect = getSize().x / getSize().y;
+                float sizeAspect = cam->getSize().x / cam->getSize().y;
+                DUMP(CAMERA_DEBUG, aspect);
+                DUMP(CAMERA_DEBUG, sizeAspect);
+                if(aspect > sizeAspect)
+                    _size = sf::Vector2f(cam->getSize().y * aspect, cam->getSize().y);
+                else
+                    _size = sf::Vector2f(cam->getSize().x, cam->getSize().x / aspect);
+            }
+            break;
+        case ScalingMode::XLocked:
+            {
+                float aspect = getSize().x / getSize().y;
+                _size = sf::Vector2f(cam->getFOV(), cam->getFOV() / aspect);
+            }
+            break;
+        case ScalingMode::YLocked:
+            {
+                float aspect = getSize().x / getSize().y;
+                _size = sf::Vector2f(cam->getFOV() * aspect, cam->getFOV());
+            }
+            break;
+        case ScalingMode::Scaled:
+            {
+                _size = cam->getSize();
+            }
+            break;
+        }
+
+        view.setSize(_size);
         target.setView(view);
     }
     else
