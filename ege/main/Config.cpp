8983f725dea517ccbbbd7aaeaa80265465a62262
Maciej Zygmanowski <sppmacd@pm.me>
2020-10-13 16:10:01 +0200

    ASSERT: Call abort() when assert fails
    It allows us to debug assertion fails much easier (we don't
    need to set breakpoint each time)

--- ege/main/Config.cpp
+++ ege/main/Config.cpp
@@ -10,5 +10,5 @@ Copyright (c) Sppmacd 2020
 void _ege_assertion_failed(const char* expr, const char* file, unsigned line)
 {
     fprintf(stderr, "0003 EGE/main: \e[31;1mASSERTION FAILED:\e[0m [ %s ] -- In \e[33m%s\e[0m:\e[95m%d\e[0m\n", expr, file, line);
-    exit(1234);
+    abort();
 }
