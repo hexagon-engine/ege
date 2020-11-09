b529a8ce3240db05e306991bf701ad76f0ac44f8
Maciej Zygmanowski <sppmacd@pm.me>
2020-11-09 12:57:54 +0100

    tilemap: Allow (and require) setting tile size in TileMap2D
    It's required for TilemapRenderer.

--- ege/tilemap/TileMap2D.h
+++ ege/tilemap/TileMap2D.h
@@ -16,8 +16,14 @@ class TileMap2D
 public:
     typedef TT TileType;
 
+    const EGE::Vec2u getTileSize() const { return m_tileSize; }
+    void setTileSize(const EGE::Vec2u& vec) { m_tileSize = vec; }
+
     virtual TileType* getTile(EGE::Vector2<CoordType> vec) = 0;
     virtual TileType& ensureTile(EGE::Vector2<CoordType> vec) = 0;
+
+private:
+    EGE::Vec2u m_tileSize;
 };
 
 }
