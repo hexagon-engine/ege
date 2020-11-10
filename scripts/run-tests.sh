6174c116d3fcfb94a2f7681b2d8d9eab1c7cedfe
Maciej Zygmanowski <sppmacd@pm.me>
2020-11-10 13:12:23 +0100

    scripts: Try fix run-tests.sh

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,5 +1,13 @@
 #/bin/bash
 
+PID=0
+
+function kill_process() {
+  kill -9 ${PID}
+}
+
+trap kill_process SIGINT
+
 printf "\n\n"
 
 if [ -z ${EGE_ROOT+x} ]; then
@@ -9,12 +17,19 @@ fi
 
 fail=0
 for test in ${EGE_ROOT}/build/root/tests/*; do
-	if ${test}; then
+	cd build/root
+	
+	${test} &
+	PID="$!"
+	fg
+	
+	if [ $? == 0 ]; then
 		printf -- "\e[32mPASS:\e[0m \e[1m%s\e[0m\n" "${test}"
 	else
 		printf -- "\e[31mFAIL:\e[0m \e[1m%s\e[0m\n" "${test}"
 		fail=1
 	fi
+	cd ../..
 done
 
 printf "\n\n"
