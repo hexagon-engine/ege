7c6dbf2c17c176324c18eb4dc176c7438d697c4c
Maciej Zygmanowski <sppmacd@pm.me>
2020-11-17 14:35:18 +0100

    util.h: Sort #includes

--- ege/util.h
+++ ege/util.h
@@ -6,28 +6,28 @@ Copyright (c) Sppmacd 2020
 #pragma once
 
 #include <ege/util/Converter.h>
-#include <ege/util/Math.h>
-#include <ege/util/ObjectInt.h>
-#include <ege/util/ObjectString.h>
-#include <ege/util/Serializable.h>
-#include <ege/util/Vector.h>
 #include <ege/util/Equation.h>
-#include <ege/util/ObjectArray.h>
-#include <ege/util/ObjectList.h>
-#include <ege/util/ObjectUnsignedInt.h>
-#include <ege/util/StringUtils.h>
 #include <ege/util/EquationSystem.h>
-#include <ege/util/ObjectFloat.h>
-#include <ege/util/ObjectMap.h>
-#include <ege/util/PointerUtils.h>
 #include <ege/util/Geometry.h>
 #include <ege/util/JSONConverter.h>
+#include <ege/util/Math.h>
 #include <ege/util/Object.h>
+#include <ege/util/ObjectArray.h>
 #include <ege/util/ObjectBoolean.h>
+#include <ege/util/ObjectFloat.h>
+#include <ege/util/ObjectInt.h>
+#include <ege/util/ObjectList.h>
+#include <ege/util/ObjectMap.h>
 #include <ege/util/ObjectSerializers.h>
+#include <ege/util/ObjectString.h>
+#include <ege/util/ObjectUnsignedInt.h>
+#include <ege/util/PointerUtils.h>
 #include <ege/util/Random.h>
+#include <ege/util/Serializable.h>
+#include <ege/util/StringUtils.h>
 #include <ege/util/Time.h>
 #include <ege/util/Types.h>
+#include <ege/util/Vector.h>
 
 #include <ege/util/system.h>
 
