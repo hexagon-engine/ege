00ce89999d6b3dbb5bf356e1f93a831a82da3c6a
Maciej Zygmanowski <sppmacd@pm.me>
2020-12-29 16:17:14 +0100

    ege: Add convenience headers for gui and scene

---
+++ ege/gui.h
@@ -0,0 +1,24 @@
+/*
+EGE - Extendable Game Engine
+Copyright (c) Sppmacd 2020
+*/
+
+#pragma once
+
+#include <ege/gui/Animatable.h>
+#include <ege/gui/AnimationEasingFunctions.h>
+#include <ege/gui/Animation.h>
+#include <ege/gui/Button.h>
+#include <ege/gui/CheckBox.cpp>
+#include <ege/gui/CheckBox.h>
+#include <ege/gui/CompoundWidget.h>
+#include <ege/gui/Frame.h>
+#include <ege/gui/GameLoop.h>
+#include <ege/gui/GUIGameLoop.h>
+#include <ege/gui/GUIResourceManager.h>
+#include <ege/gui/GUIScreen.h>
+#include <ege/gui/Label.h>
+#include <ege/gui/RadioButton.h>
+#include <ege/gui/ScrollBar.h>
+#include <ege/gui/TextBox.h>
+#include <ege/gui/Widget.h>
---
+++ ege/scene.h
@@ -0,0 +1,19 @@
+/*
+EGE - Extendable Game Engine
+Copyright (c) Sppmacd 2020
+*/
+
+#pragma once
+
+#include <ege/scene/CameraObject2D.h>
+#include <ege/scene/CameraObject.h>
+#include <ege/scene/DummyObject2D.h>
+#include <ege/scene/ObjectRenderer.h>
+#include <ege/scene/ParticleSystem2D.h>
+#include <ege/scene/Scene2D.h>
+#include <ege/scene/Scene.h>
+#include <ege/scene/SceneObject2D.h>
+#include <ege/scene/SceneObject.h>
+#include <ege/scene/SceneWidget.h>
+#include <ege/scene/TexturedRenderer2D.h>
+#include <ege/scene/TilemapRenderer2D.h>
