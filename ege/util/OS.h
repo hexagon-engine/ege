952927793e9948deafa3d6b32d6cb6a0fe7424a9
Maciej Zygmanowski <sppmacd@pm.me>
2021-03-28 13:05:13 +0200

    util: Add macros for OS detection

---
+++ ege/util/OS.h
@@ -0,0 +1,58 @@
+/*
+*
+*   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*
+*      ,----  ,----  ,----
+*      |      |      |
+*      |----  | --,  |----
+*      |      |   |  |
+*      '----  '---'  '----
+*
+*     Framework Library for Hexagon
+*
+*    Copyright (c) Sppmacd 2020 - 2021
+*
+*    Permission is hereby granted, free of charge, to any person obtaining a copy
+*    of this software and associated documentation files (the "Software"), to deal
+*    in the Software without restriction, including without limitation the rights
+*    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*    copies of the Software, and to permit persons to whom the Software is
+*    furnished to do so, subject to the following conditions:
+*
+*    The above copyright notice and this permission notice shall be included in all
+*    copies or substantial portions of the Software.
+*
+*    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*    SOFTWARE.
+*
+*   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*
+*/
+
+#pragma once
+
+// Windows
+#ifdef _WIN32
+    #define EGE_OS_WINDOWS
+    #define EGE_API_WIN32
+#endif
+
+// MacOS
+#if defined(__APPLE__) && defined(__MACH__)
+    #define EGE_OS_MACOS
+#endif
+
+// Unix-like
+#if defined(__unix__)
+    #define EGE_API_UNIX
+#endif
+
+// Linux
+#ifdef __linux__
+    #define EGE_OS_LINUX
+#endif
