2b6c86abc5e5079c402b014cb5388db1c3c55bc9
Maciej Zygmanowski <sppmacd@pm.me>
2021-04-11 15:34:23 +0200

    scene: Make complexloader test code shorter

--- ege/scene/tests/complexloader.cpp
+++ ege/scene/tests/complexloader.cpp
@@ -3,24 +3,6 @@
 #include <ege/gui.h>
 #include <ege/scene.h>
 
-class CLBlock : public EGE::SceneObject2D
-{
-public:
-    EGE_SCENEOBJECT("CLBlock");
-
-    CLBlock(EGE::Scene& scene)
-    : EGE::SceneObject2D(scene) {}
-};
-
-class CLPlayer : public EGE::SceneObject2D
-{
-public:
-    EGE_SCENEOBJECT("CLPlayer");
-
-    CLPlayer(EGE::Scene& scene)
-    : EGE::SceneObject2D(scene) {}
-};
-
 TESTCASE(complexLoader)
 {
     EGE::GUIGameLoop loop;
@@ -30,8 +12,6 @@ TESTCASE(complexLoader)
     auto scene = make<EGE::Scene2D>(&loop);
 
     EGE::SceneObjectRegistry& sceneObjects = scene->getRegistry();
-    sceneObjects.addType2D<CLBlock>();
-    sceneObjects.addType2D<CLPlayer>();
     if(!sceneObjects.loadFromFile("objects/complexloader.json"))
         return 1;
 
@@ -40,15 +20,13 @@ TESTCASE(complexLoader)
 
     // Spawn player
     // TODO: Make better API for this!
-    auto player = dynamic_cast<CLPlayer*>(scene->getObjectByName("player"));
+    auto player = dynamic_cast<EGE::SceneObject2D*>(scene->getObjectByName("player"));
     if(!player)
-        player = scene->addNewObject<CLPlayer>().get();
-
-    ege_log.info() << player;
+        player = dynamic_cast<EGE::SceneObject2D*>(scene->addNewObject("CLPlayer").get());
 
     // Setup keybinds
     auto keybinds = make<EGE::KeybindManager>();
-    keybinds->addTrigger("place", sf::Mouse::Left, [scene, player]{ scene->addNewObject<CLBlock>()->setPosition(player->getPosition()); });
+    keybinds->addTrigger("place", sf::Mouse::Left, [scene, player]{ EGE::SceneObject::cast<EGE::SceneObject2D>(scene->addNewObject("CLBlock"))->setPosition(player->getPosition()); });
     keybinds->addSwitch("L", sf::Keyboard::A, [player](bool p) { player->setMotion({-1*p, player->getMotion().y}); });
     keybinds->addSwitch("R", sf::Keyboard::D, [player](bool p) { player->setMotion({1*p, player->getMotion().y}); });
     keybinds->addSwitch("U", sf::Keyboard::W, [player](bool p) { player->setMotion({player->getMotion().x, -1*p}); });
