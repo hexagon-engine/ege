1229d49542d88803a6c88b0d1f34acd408f0ddbd
Maciej Zygmanowski <sppmacd@pm.me>
2021-07-17 13:08:35 +0200

    util/system: Fix logic error in ExactTime
    In seconds() there was division of nanoseconds by 1'000'000 instead of
    1'000'000'000 to get seconds.
    
    This commit also uses fancy number notation (with ') for better
    readability.

--- ege/util/system/Time.h
+++ ege/util/system/Time.h
@@ -49,19 +49,19 @@ class ExactTime
 {
 public:
     static inline ExactTime zero() { return {}; }
-    static inline ExactTime fromSeconds(double s) { return ExactTime(s, (s - std::floor(s)) * 1000000000); }
-    static inline ExactTime fromMilliseconds(long long ms) { return ExactTime(ms / 1000, ms * 1000000); }
-    static inline ExactTime fromMicroseconds(long long us) { return ExactTime(us / 1000000, us * 1000); }
-    static inline ExactTime fromNanoseconds(long long ns) { return ExactTime(ns / 1000000000, ns); }
-    static inline ExactTime fromSecondsAndNanoseconds(time_t s, long long ns) { return ExactTime(s, ns % 1000000000); }
+    static inline ExactTime fromSeconds(double s) { return ExactTime(s, (s - std::floor(s)) * 1'000'000'000); }
+    static inline ExactTime fromMilliseconds(long long ms) { return ExactTime(ms / 1'000, ms * 1'000'000); }
+    static inline ExactTime fromMicroseconds(long long us) { return ExactTime(us / 1'000'000, us * 1'000); }
+    static inline ExactTime fromNanoseconds(long long ns) { return ExactTime(ns / 1'000'000'000, ns); }
+    static inline ExactTime fromSecondsAndNanoseconds(time_t s, long long ns) { return ExactTime(s, ns % 1'000'000'000); }
 
     ExactTime roundToSeconds() const { return ExactTime(m_seconds, 0); }
     ExactTime stripSeconds() const { return ExactTime(0, m_nanoseconds); }
 
-    double seconds() const { return m_seconds + m_nanoseconds / 1000000.0; }
-    double milliseconds() const { return m_seconds * 1000 + m_nanoseconds / 1000000.0; }
-    double microseconds() const { return m_seconds * 1000000 + m_nanoseconds / 1000.0; }
-    long long nanoseconds() const { return m_seconds * 1000000000 + m_nanoseconds; }
+    double seconds() const { return m_seconds + m_nanoseconds / 1'000'000'000.0; }
+    double milliseconds() const { return m_seconds * 1'000 + m_nanoseconds / 1'000'000.0; }
+    double microseconds() const { return m_seconds * 1'000'000 + m_nanoseconds / 1'000.0; }
+    long long nanoseconds() const { return m_seconds * 1'000'000'000 + m_nanoseconds; }
 
     time_t onlySeconds() const { return m_seconds; }
     long long onlyNanoseconds() const { return m_nanoseconds; }
