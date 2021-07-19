227d998b7d0a6659ce858265e5d41103c1c1bad7
Maciej Zygmanowski <sppmacd@pm.me>
2021-07-19 17:54:59 +0200

    gui: Add `gfx` test
    This test tests `gfx` module capabilities. The gfx module cannot test
    itself because it is not allowed to create GUI.
    
    Currently the test is testing only renderTextWithBackground().

---
+++ ege/gui/tests/gfx.cpp
@@ -0,0 +1,49 @@
+#include <testsuite/Tests.h>
+#include <ege/gui.h>
+
+class TestWidget : public EGE::Widget
+{
+public:
+    TestWidget(EGE::Widget& parent)
+    : EGE::Widget(parent) {}
+
+    virtual void updateGeometry(EGE::Renderer&) override
+    {
+        if(!m_font)
+        {
+            m_font = getLoop().getResourceManager()->getDefaultFont().get();
+            ASSERT(m_font);
+        }
+    }
+
+    virtual void render(EGE::Renderer& renderer) const override
+    {
+        EGE::Renderer::TextWithBackgroundSettings settings;
+        settings.color = EGE::Colors::white;
+        settings.font_size = 15;
+        settings.padding = 5;
+        settings.text_align = EGE::Renderer::TextAlign::Center;
+        renderer.renderTextWithBackground(getSize().x / 2, getSize().y / 2, *m_font, L"test 1234 abdegeąəęóą", settings);
+    }
+
+private:
+    sf::Font* m_font = nullptr;
+};
+
+TESTCASE(renderTextWithBackground)
+{
+    EGE::GUIGameLoop loop;
+
+    auto resourceManager = make<EGE::GUIResourceManager>();
+    resourceManager->registerDefaultFont("font.ttf");
+    loop.setResourceManager(resourceManager);
+
+    auto guiScreen = make<EGE::GUIScreen>(loop);
+    guiScreen->addNewWidget<TestWidget>();
+    loop.setCurrentGUIScreen(guiScreen);
+
+    loop.openWindow(sf::VideoMode(500, 500), "renderTextWithBackground");
+    return loop.run();
+}
+
+RUN_TESTS(gfx)
