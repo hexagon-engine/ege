97c1cecfdf8b26280e01eca04fcfca1b6e0c9e1e
Maciej Zygmanowski <sppmacd@pm.me>
2021-07-28 16:53:13 +0200

    ege3d: Add some OpenGL error checking macro

---
+++ ege/ege3d/ege3d/window/GLError.cpp
@@ -0,0 +1,44 @@
+/*
+    EGE3d - 3D rendering engine for Hexagon
+
+    Copyright (c) 2020 Hexagon Engine
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+#include "GLError.h"
+
+#include <ege/debug/Logger.h>
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+namespace EGE3d
+{
+
+void printGLError(char const* call)
+{
+    auto error = glGetError();
+    if(error != GL_NO_ERROR)
+    {
+        ege_log.error() << "OpenGL Error when calling '" << call << "': " << gluErrorString(error);
+    }
+}
+
+}
