5134d5f6eb5653770d859f939a45f9c2b5c36e53
Maciej Zygmanowski <sppmacd@pm.me>
2021-07-29 16:21:08 +0200

    ege3d: Add basic support for GL extensions

---
+++ ege/ege3d/ege3d/window/GLEW.cpp
@@ -0,0 +1,55 @@
+/*
+    EGE3d - 3D rendering engine for Hexagon
+
+    Copyright (c) 2020 Hexagon Engine
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+#include "GLEW.h"
+
+#include <ege/debug/Logger.h>
+
+#include <GL/glew.h>
+
+namespace EGE3d
+{
+
+static bool s_glewInitialized = false;
+
+static void initGLEWIfNeeded()
+{
+    GLenum rc = glewInit();
+    if(rc != GLEW_OK)
+    {
+        ege_log.critical() << "EGE3d: Failed to initialize GLEW. OpenGL extensions will not be supported (" << glewGetErrorString(rc) << ")";
+        return;
+    }
+    s_glewInitialized = true;
+}
+
+bool hasGLExtension(EGE::String const& name)
+{
+    initGLEWIfNeeded();
+    if(!s_glewInitialized)
+        return false;
+    return glewIsExtensionSupported(name.c_str());
+}
+
+}
